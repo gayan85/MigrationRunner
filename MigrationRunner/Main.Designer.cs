12aa8695c473954f28658232c5d88fcd9129ffad
Gayan Ranasinghe <gayan.ranasinghe@solid.lk>
2016-08-10 18:55:56 +0530

    init repo migrator

---
+++ MigrationRunner/Main.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace MigrationRunner
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMigrationUp = new System.Windows.Forms.Button();
+            this.txtAssemblyPath = new System.Windows.Forms.TextBox();
+            this.btnLoadMigration = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveConfigurationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMigrationUp
+            // 
+            this.btnMigrationUp.Location = new System.Drawing.Point(403, 132);
+            this.btnMigrationUp.Name = "btnMigrationUp";
+            this.btnMigrationUp.Size = new System.Drawing.Size(158, 43);
+            this.btnMigrationUp.TabIndex = 0;
+            this.btnMigrationUp.Text = "Migration Up";
+            this.btnMigrationUp.UseVisualStyleBackColor = true;
+            this.btnMigrationUp.Click += new System.EventHandler(this.btnMigrationUp_Click);
+            // 
+            // txtAssemblyPath
+            // 
+            this.txtAssemblyPath.Location = new System.Drawing.Point(12, 45);
+            this.txtAssemblyPath.Name = "txtAssemblyPath";
+            this.txtAssemblyPath.ReadOnly = true;
+            this.txtAssemblyPath.Size = new System.Drawing.Size(468, 20);
+            this.txtAssemblyPath.TabIndex = 1;
+            // 
+            // btnLoadMigration
+            // 
+            this.btnLoadMigration.Location = new System.Drawing.Point(486, 43);
+            this.btnLoadMigration.Name = "btnLoadMigration";
+            this.btnLoadMigration.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadMigration.TabIndex = 2;
+            this.btnLoadMigration.Text = "Load";
+            this.btnLoadMigration.UseVisualStyleBackColor = true;
+            this.btnLoadMigration.Click += new System.EventHandler(this.btnLoadMigration_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(71, 83);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(255, 20);
+            this.txtServer.TabIndex = 3;
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(71, 107);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(255, 20);
+            this.txtDatabase.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(71, 155);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(255, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(71, 131);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(255, 20);
+            this.txtUsername.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(573, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveConfigurationToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // saveConfigurationToolStripMenuItem
+            // 
+            this.saveConfigurationToolStripMenuItem.Name = "saveConfigurationToolStripMenuItem";
+            this.saveConfigurationToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.saveConfigurationToolStripMenuItem.Text = "Save Configuration";
+            this.saveConfigurationToolStripMenuItem.Click += new System.EventHandler(this.saveConfigurationToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 188);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtDatabase);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnLoadMigration);
+            this.Controls.Add(this.txtAssemblyPath);
+            this.Controls.Add(this.btnMigrationUp);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Runner";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMigrationUp;
+        private System.Windows.Forms.TextBox txtAssemblyPath;
+        private System.Windows.Forms.Button btnLoadMigration;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveConfigurationToolStripMenuItem;
+    }
+}
+
---
+++ MigrationRunner/Main.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MigrationRunner
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            _openFileDialog = new OpenFileDialog() { Filter = @"Migration dll file|*.dll" };
+            LoadSettings();
+        }
+
+        private string _assemblyPath;
+        private string _server;
+        private string _database;
+        private string _user;
+        private string _password;
+        private readonly OpenFileDialog _openFileDialog;
+
+        private void btnMigrationUp_Click(object sender, EventArgs e)
+        {
+            ReloadSettings();
+            if (InvalidateSettings()) return;
+            var announcer = new FluentMigrator.Runner.Announcers.TextWriterAnnouncer(s => System.Diagnostics.Debug.WriteLine(s));
+            var assembly = Assembly.LoadFile(_assemblyPath);
+            var migrationContext = new FluentMigrator.Runner.Initialization.RunnerContext(announcer)
+            {
+                Namespace = assembly.GetTypes().First(a => a.Name.ToLower().StartsWith("step")).Namespace,
+                TransactionPerSession = false,
+                ApplicationContext = "sqlserver",
+                Targets = new[] { _assemblyPath },
+            };
+
+            var options = new FluentMigrator.Runner.Processors.ProcessorOptions()
+            {
+                PreviewOnly = false,
+                Timeout = 600
+            };
+
+            var connectionString = $"Data Source={_server};User ID={_user};Password={_password};Database={_database};";
+            var factory = new FluentMigrator.Runner.Processors.SqlServer.SqlServerProcessorFactory();
+            try
+            {
+                using (var processor = factory.Create(connectionString, announcer, options))
+                {
+                    var runner = new FluentMigrator.Runner.MigrationRunner(assembly, migrationContext, processor);
+                    runner.MigrateUp(true);
+                    MessageBox.Show(@"Database Migrate succeeded!!", @"Success", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, @"Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool InvalidateSettings()
+        {
+
+            if (string.IsNullOrEmpty(_assemblyPath))
+            {
+                MessageBox.Show(@"Assembly path required", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(_server))
+            {
+                MessageBox.Show(@"Server name required", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
+            if (string.IsNullOrEmpty(_database))
+            {
+                MessageBox.Show(@"Database name required", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
+            if (string.IsNullOrEmpty(_user))
+            {
+                MessageBox.Show(@"Sql login user required", @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
+            if (string.IsNullOrEmpty(_password))
+            {
+                MessageBox.Show(@"Sql login password required!!", @"Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private void LoadSettings()
+        {
+            txtUsername.Text = Properties.Settings.Default.User;
+            txtDatabase.Text = Properties.Settings.Default.Database;
+            txtServer.Text = Properties.Settings.Default.Server;
+            txtPassword.Text = Properties.Settings.Default.Password;
+            ReloadSettings();
+        }
+
+        private void ReloadSettings()
+        {
+            _user = txtUsername.Text;
+            _server = txtServer.Text;
+            _assemblyPath = _openFileDialog.FileName;
+            _database = txtDatabase.Text;
+            _password = txtPassword.Text;
+        }
+
+        private void btnLoadMigration_Click(object sender, EventArgs e)
+        {
+
+            var result = _openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _assemblyPath = _openFileDialog.FileName;
+                txtAssemblyPath.Text = _assemblyPath;
+            }
+            Console.WriteLine(result);
+        }
+
+        private void saveConfigurationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReloadSettings();
+            Properties.Settings.Default.User = _user;
+            Properties.Settings.Default.Database = _database;
+            Properties.Settings.Default.Server = _server;
+            Properties.Settings.Default.Password = _password;
+            Properties.Settings.Default.Save();
+            MessageBox.Show(@"Configuration save succeeded!!");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
