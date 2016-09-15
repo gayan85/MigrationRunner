b267d10b577e9fffba1d0b8da2e3431974cee39f
Gayan Ranasinghe <gayan.ranasinghe@solid.lk>
2016-09-15 16:34:16 +0530

    added unit tests

---
+++ libs/MigrationRunner.Test/Gui.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MigrationRunner.Helpers;
+using NUnit.Framework;
+
+namespace MigrationRunner.Test
+{
+    [TestFixture]
+    public class Gui
+    {
+        [Test]
+        public void Form_Controls_Enable()
+        {
+            var main = new Main();
+            Assert.DoesNotThrow(() =>
+            {
+                main.Enable(true);
+            });
+        }
+
+        [Test]
+        public void Form_Controls_Disabled()
+        {
+            var main = new Main();
+            Assert.DoesNotThrow(() =>
+            {
+                main.Enable(false);
+            });
+        }
+    }
+}
